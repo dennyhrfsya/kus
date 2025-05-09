82e32807dd8f2b8b04294e48ea6aaa8e82a41f4d
dennyhrfsya <dennyherfansya10@gmail.com>
2025-05-09 15:00:02 +0700

    KUS

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  //  content: ["./src/**/*.{html,js}"],
+  content: ["index.html"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      fontFamily: {
+        serif: ['"Source Serif 4"', "serif"],
+      },
+      colors: {
+        boltgunmetal: "#383838",
+        dragonfire: "#F66B34",
+        margarine: "#F2D639",
+        choco: "#78350f",
+      },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};

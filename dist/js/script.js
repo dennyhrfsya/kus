d604b65ae495620e8562515f6b1b55279ec7c5f6
dennyhrfsya <dennyherfansya10@gmail.com>
2025-05-16 11:00:46 +0700

    Update 2

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,33 +1,33 @@
 // Hamburger
-const hamburger = document.querySelector("#hamburger");
-const navMenu = document.querySelector("#nav-menu");
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
 
-hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle("hamburger-active");
-  navMenu.classList.toggle("hidden");
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('hidden');
 });
 
 // Klik di luar Hamburger
-window.addEventListener("click", function (e) {
+window.addEventListener('click', function (e) {
   if (e.target != hamburger && e.target != navMenu) {
-    hamburger.classList.remove("hamburger-active");
-    navMenu.classList.add("hidden");
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
   }
 });
 
 // Navbar Fixed
 window.onscroll = function () {
-  const header = document.querySelector("header");
+  const header = document.querySelector('header');
   const fixedNav = header.offsetTop;
-  const toTop = document.querySelector("#to-top");
+  const toTop = document.querySelector('#to-top');
 
   if (window.scrollY > fixedNav) {
-    header.classList.add("navbar-fixed");
-    toTop.classList.remove("hidden");
-    toTop.classList.add("flex");
+    header.classList.add('navbar-fixed');
+    toTop.classList.remove('hidden');
+    toTop.classList.add('flex');
   } else {
-    header.classList.remove("navbar-fixed");
-    toTop.classList.remove("flex");
-    toTop.classList.add("hidden");
+    header.classList.remove('navbar-fixed');
+    toTop.classList.remove('flex');
+    toTop.classList.add('hidden');
   }
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   //  content: ["./src/**/*.{html,js}"],
-  content: ["index.html"],
-  darkMode: "class",
+  content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
-      padding: "16px",
+      padding: '16px',
     },
     extend: {
       fontFamily: {
-        serif: ['"Source Serif 4"', "serif"],
+        serif: ['"Source Serif 4"', 'serif'],
       },
       colors: {
-        boltgunmetal: "#383838",
-        dragonfire: "#F66B34",
-        margarine: "#F2D639",
-        choco: "#78350f",
+        boltgunmetal: '#383838',
+        dragonfire: '#F66B34',
+        margarine: '#F2D639',
+        choco: '#78350f',
       },
       animation: {
-        "spin-slow": "spin 3s linear infinite",
+        'spin-slow': 'spin 3s linear infinite',
       },
     },
   },
-  plugins: [require("tailwindcss-animated")],
+  plugins: [require('tailwindcss-animated')],
 };
